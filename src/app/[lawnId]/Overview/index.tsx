1f1a22c0b8ccda299136a175e0bdd25ea9cb6b5a
dtpietrzak <dtpietrzak@gmail.com>
2024-05-22 23:50:16 -0500

    overview improvements

--- src/app/[lawnId]/Overview/index.tsx
+++ src/app/[lawnId]/Overview/index.tsx
@@ -2,9 +2,9 @@ import { Card, Divider, Flex, Table, Text, Title } from "@mantine/core";
 import { FC } from "react";
 import TabContainer from "../_components/tabs/TabContainer";
 import useLawnData, { LawnData, LawnEvent } from "@/_hooks/useLawnData";
-import useWeatherData, { WeatherApiMappedData } from "@/_hooks/useWeatherData";
-import { IconArrowLeft, IconArrowRight, IconArrowUp, IconDroplet, IconPlant2 } from "@tabler/icons-react";
-import { differenceInHours, differenceInMinutes, format } from 'date-fns'
+import useWeatherData, { MappedDataItem, ProcessedMappedDataItem } from "@/_hooks/useWeatherData";
+import { IconArrowLeft, IconArrowRight, IconArrowUp, IconBladeFilled, IconDroplet, IconPlant2, IconTractor } from "@tabler/icons-react";
+import { differenceInHours, format } from 'date-fns'
 
 export type OverviewProps = {
 }
@@ -13,8 +13,12 @@ const Overview: FC<OverviewProps> = () => {
   const { lastMow, viewingLawn } = useLawnData()
   const { weatherData, transformedData } = useWeatherData()
 
-  const history = weatherData.weatherApi?.history.forecast.forecastday
-  const forecast = weatherData.weatherApi?.forecast.forecast.forecastday
+  const todayAgg = transformedData.processedData.filter((x) => {
+    const today = new Date(new Date().setHours(0, 0, 0, 0)).getTime()
+    const todayFormatted = format(new Date(today), 'M-d')
+    const dateFormatted = format(x.epoch, 'M-d')
+    if (todayFormatted === dateFormatted) return true
+  })?.[0] ?? null
 
   return (
     <TabContainer>
@@ -29,36 +33,49 @@ const Overview: FC<OverviewProps> = () => {
                 <Text size="sm">
                   Mow Date: {lastMow.datetime.toDateString()}
                 </Text>
-                <Text size="sm">
-                  Projected Height: { }
-                </Text>
+                {
+                  todayAgg ?
+                    <Text size="sm">
+                      End of Day Projected Height: {(lastMow.meta.height + parseFloat(todayAgg.inchesGrownAccumulator)).toFixed(2)}{'"'}
+                    </Text>
+                    : null
+                }
               </Flex>
             </Card>
           </>
           :
           <></>
       }
+      <Flex direction='row' justify='space-evenly' w='100%' gap='sm'>
+        <Card>
+          <Title size='h4'>
+            Rain Gauges
+          </Title>
+          <Flex mt={8}>
+            <RainGauge
+              backward
+              array={transformedData.weatherApiMapped}
+            />
+          </Flex>
+        </Card>
+        <Card>
+          <Title size='h4'>
+            &nbsp;
+          </Title>
+          <Flex mt={8}>
+            <RainGauge
+              foreward
+              array={transformedData.weatherApiMapped}
+            />
+          </Flex>
+        </Card>
+      </Flex>
       <Card>
         <Title size='h4'>
-          Rain Gauge
-        </Title>
-        <Flex mt={8}>
-          <RainGauge
-            backward
-            array={transformedData.weatherApiMapped}
-          />
-          <RainGauge
-            foreward
-            array={transformedData.weatherApiMapped}
-          />
-        </Flex>
-      </Card>
-      <Card>
-        <Title size='h4'>
-          AGDU Gauge
+          Mow Gauge
         </Title>
         <GrassGauge
-          array={transformedData.weatherApiMapped}
+          array={transformedData.processedData}
           viewingLawn={viewingLawn}
           lastMow={lastMow}
         />
@@ -68,7 +85,7 @@ const Overview: FC<OverviewProps> = () => {
 }
 
 type RainGaugeProps = {
-  array: WeatherApiMappedData[]
+  array: MappedDataItem[]
   backward?: boolean
   foreward?: boolean
 }
@@ -107,51 +124,52 @@ const RainGauge: FC<RainGaugeProps> = ({
           </>
         }
       </Flex>
-      <Flex
-        direction='row'
-        columnGap={8}
-        justify='space-between'
-      >
-        <Text size="xs" c="blue">
-          {`${filtered.reduce((acc, cur) => {
-            return acc + cur.rain
-          }, 0).toFixed(2)}"`}
-        </Text>
-        <Text size="xs" c='blue' opacity={0.5}>
-          7 day
-        </Text>
-      </Flex>
-      {
-        filtered.map((day) => {
-          const date = format(day.epoch, 'M-d')
-          const currentDate = format(new Date(), 'M-d')
+      <Table striped highlightOnHover w={120}>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>
+              <Text size="xs" c='blue' opacity={0.5}>
+                <b>7 day</b>
+              </Text>
+            </Table.Th>
+            <Table.Th>
+              <Text size="xs" c="blue">
+                <b>{`${filtered.reduce((acc, cur) => {
+                  return acc + cur.rain
+                }, 0).toFixed(2)}"`}</b>
+              </Text>
+            </Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        {
+          filtered.map((day) => {
+            const date = format(day.epoch, 'M-d')
+            const currentDate = format(new Date(), 'M-d')
 
-          return (
-            <div key={day.epoch}>
-              <Flex
-                direction='row'
-                columnGap={8}
-                justify='space-between'
-                className="w-full"
-              >
-                <Text size="xs">
-                  {`${day.rain}"`}
-                </Text>
-                <Text size="xs" c={date === currentDate ? 'white' : 'dimmed'}>
-                  {date}
-                </Text>
-              </Flex>
-              <Divider />
-            </div>
-          )
-        })
-      }
+            return (
+              <Table.Tr key={day.epoch}>
+                <Table.Td>
+                  <Text size="xs" c={date === currentDate ? 'white' : 'dimmed'}>
+                    {date}
+                  </Text>
+                </Table.Td>
+                <Table.Td>
+                  <Text size="xs">
+                    {`${day.rain}"`}
+                  </Text>
+                </Table.Td>
+                <Divider />
+              </Table.Tr>
+            )
+          })
+        }
+      </Table>
     </Flex>
   )
 }
 
 type GrassGaugeProps = {
-  array?: WeatherApiMappedData[]
+  array?: ProcessedMappedDataItem[]
   viewingLawn?: LawnData
   lastMow?: LawnEvent<'mow'>
 }
@@ -162,53 +180,6 @@ const GrassGauge: FC<GrassGaugeProps> = ({
   if (!array || !viewingLawn?.properties.mow || !lastMow?.id) {
     return <>Loading...</>
   }
-
-  const today = new Date(new Date().setHours(0, 0, 0, 0)).getTime()
-  const todayFormatted = format(new Date(today), 'M-d')
-  const lastMowDay = new Date(new Date(lastMow.datetime).setHours(0, 0, 0, 0)).getTime()
-  const lastMowDayFormatted = format(new Date(lastMowDay), 'M-d')
-  const partialDay = differenceInHours(lastMow.datetime, lastMowDay) / 24
-  const filtered = array.filter((x) => (
-    x.epoch >= today || x.epoch >= lastMowDay
-  ))
-
-  let inchesGrownAccumulator = 0
-  let percentCutAccumulator = 0
-
-  const processed = filtered.map((x) => {
-    if (!x.gdd_v2 || !x.agdu_p1) return null
-    const dateFormatted = format(x.epoch, 'M-d')
-    const dayOfWeek = format(x.epoch, 'EEE')
-
-    let _agdu = x.agdu_p1
-
-    if (dateFormatted === lastMowDayFormatted) {
-      _agdu = _agdu * partialDay
-    }
-
-
-
-
-    inchesGrownAccumulator += _agdu
-
-    const oneThird = viewingLawn?.properties.mow / 3
-    const ratioToCut = (inchesGrownAccumulator / oneThird) / 3
-    const percentCut = (ratioToCut * 100)
-
-    percentCutAccumulator += percentCut
-
-    return {
-      isToday: dateFormatted === todayFormatted,
-      isMowDay: dateFormatted === lastMowDayFormatted,
-      percentCut: percentCut.toFixed(0),
-      percentCutAccumulator: percentCutAccumulator.toFixed(0),
-      inchesGrown: _agdu.toFixed(2),
-      inchesGrownAccumulator: inchesGrownAccumulator.toFixed(2),
-      dateFormatted: dateFormatted,
-      dayOfWeek: dayOfWeek,
-      ...x,
-    }
-  })
 
   return (
     <Flex
@@ -234,41 +205,55 @@ const GrassGauge: FC<GrassGaugeProps> = ({
           {`${viewingLawn?.properties.mow}"`}
         </Text>
       </Flex>
-      <Table>
+      <Table striped highlightOnHover withColumnBorders>
         <Table.Thead>
           <Table.Tr>
-            <Table.Th><Text size="xs">temp</Text></Table.Th>
-            <Table.Th><Text size="xs">gro</Text></Table.Th>
-            <Table.Th><Text size="xs">cut</Text></Table.Th>
-            <Table.Th><Text size="xs">day</Text></Table.Th>
+            <Table.Th><Text size="xs">Date</Text></Table.Th>
+            <Table.Th><Text size="xs">Growth</Text></Table.Th>
+            <Table.Th><Text size="xs">Cut%</Text></Table.Th>
+            <Table.Th><Text size="xs"></Text></Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>
           {
-            processed.map((data) => {
+            array.map((data) => {
               if (!data) return null
               return (
-                <Table.Tr key={data.epoch} className="p-0">
-                  <Table.Td c='yellow' className="p-0">
-                    <div className="text-[10px]">{data.avg}</div>
-                    <div className="text-[9px] text-blue-400 flex flex-row items-center -mt-1 ml-1 p-0">
-                      <IconDroplet size={8} />{`${data.rainChance}%`}
-                    </div>
-                  </Table.Td>
-                  <Table.Td>
-                    <Text size="xs">{`${data.inchesGrownAccumulator}"`}</Text>
-                  </Table.Td>
-                  <Table.Td>
-                    <Text size="xs">{`${data.percentCutAccumulator}%`}</Text>
-                  </Table.Td>
+                <Table.Tr key={data.epoch}>
                   <Table.Td c={
                     data.isToday ? 'white' :
                       data.isMowDay ? 'rgb(0, 150, 0)' : 'dimmed'
                   }>
-                    <div className="flex flex-row justify-between">
-                      <Text size="xs">{data.dayOfWeek}</Text>
-                      <Text size="xs">{data.dateFormatted}</Text>
+                    <Text size="xs">
+                      {`${data.dayOfWeek} / ${data.dateFormatted}`}
+                      {data.isMowDay && <IconTractor size={16} className="inline items-center align-middle text-center mb-0.5 ml-1" />}
+                    </Text>
+                  </Table.Td>
+                  <Table.Td>
+                    <Text size="xs" className={colorSwitchCutPerc(data.percentCutAccumulator)}>{`${data.inchesGrownAccumulator}"`}<span className={colorSwitchDayGrowth(data.inchesGrown)}>{` (${data.inchesGrown}")`}</span></Text>
+                  </Table.Td>
+                  <Table.Td>
+                    <Text size="xs" className={colorSwitchCutPerc(data.percentCutAccumulator)}>
+                      {`${data.percentCutAccumulator}%`}
+                    </Text>
+                  </Table.Td>
+                  <Table.Td>
+                    <div className="text-[10px]">
+                      <span className="text-[rgb(120,120,120)]">
+                        {data.low.toFixed(0)}
+                      </span>
+                      <span className="text-neutral-600">{' / '}</span>
+                      <span className="text-[rgb(190,140,80)]">
+                        {data.high.toFixed(0)}
+                      </span>
                     </div>
+                    {
+                      data.rainWill && data.pastOrFuture === 'future' ?
+                        <div className="text-[9px] text-blue-400 flex flex-row items-center -mt-1 ml-1 p-0 opacity-60">
+                          <IconDroplet size={8} />{`${data.rainChance}%`}
+                        </div>
+                        : null
+                    }
                   </Table.Td>
                 </Table.Tr>
               )
@@ -280,6 +265,31 @@ const GrassGauge: FC<GrassGaugeProps> = ({
   )
 }
 
+
+
+
+const colorSwitchCutPerc = (percent: string | number) => {
+  if (typeof percent === 'string') percent = parseFloat(percent)
+  if (percent < 30) return 'text-teal-600'
+  if (percent < 50) return 'text-emerald-600'
+  if (percent < 70) return 'text-green-600'
+  if (percent < 100) return 'text-green-500'
+  if (percent < 125) return 'text-lime-500'
+  if (percent < 150) return 'text-yellow-400'
+  if (percent < 200) return 'text-amber-500'
+  if (percent < 250) return 'text-orange-600'
+  return 'text-red-600'
+}
+
+const colorSwitchDayGrowth = (percent: string | number) => {
+  if (typeof percent === 'string') percent = parseFloat(percent)
+  if (percent < 0.1) return 'text-neutral-700'
+  if (percent < 0.15) return 'text-green-900'
+  if (percent < 0.2) return 'text-green-800'
+  if (percent < 0.25) return 'text-green-700'
+  if (percent < 0.3) return 'text-green-600'
+  return 'text-green-500'
+}
 
 
 export default Overview
